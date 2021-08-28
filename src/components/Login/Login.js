ee53837095c438001b1daf1fff7c6eee8f7257b4
ashrf <ashrftvm1@gmail.com>
2021-08-28 12:05:45 +0400

    debouncing

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(()=>{
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    // debouncing and cleanup
+    const handlerTimeout =  setTimeout(()=>{
+      console.log('Checking form validity')
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    },500)
+    return () => {
+      console.log("CLEANUP")
+      clearTimeout(handlerTimeout)
+    }
   },[enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
